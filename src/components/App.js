b1d6713ef26f67671892e3e4e8dbcd108bd6c352
febiasm <trevorfebias@gmail.com>
2023-01-22 02:43:32 +0300

    2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { actors } from "../data";
+console.log(actors)
+
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <NavBar />
+  <BrowserRouter>
+      <Switch>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/directors">
+          <Directors />
+        </Route>
+        <Route path="/actors">
+          <Actors />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      </BrowserRouter>
+  </div>;
 }
 
 export default App;
